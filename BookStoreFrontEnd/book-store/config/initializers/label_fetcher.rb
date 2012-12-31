6c3fcc8b3a0c28110e04594281c653a6fe345a74
Guilherme Simoes <guilherme.rdems@gmail.com>
2012-12-31 01:13:37 +0000

    Trying to improve search results.

--- BookStoreFrontEnd/book-store/config/initializers/label_fetcher.rb
+++ BookStoreFrontEnd/book-store/config/initializers/label_fetcher.rb
@@ -1,19 +1,53 @@
 include Ontology
 
-LABELS = {}
 
+CLASSES = {}
 hash = Ontology.query(" PREFIX book: <http://www.owl-ontologies.com/book.owl#>
                         PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
                         SELECT ?class ?label
-                        WHERE { ?class rdfs:label ?label }
-                          ")
+                        WHERE { ?class a rdfs:Class .
+                                ?class rdfs:label ?label
+                                FILTER NOT EXISTS { ?class rdfs:subClassOf book:Format }
+                              }
+                      ")
 hash['results']['bindings'].each do |resource|
   key = resource['label']['value'].downcase
   value = resource['class']['value']
-  LABELS[key] = value
+  CLASSES[key] = value
 end
 
-KLASSES = {
+
+FORMATS = {}
+hash = Ontology.query(" PREFIX book: <http://www.owl-ontologies.com/book.owl#>
+                        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+                        SELECT ?class ?label
+                        WHERE { ?class rdfs:subClassOf book:Format .
+                                ?class rdfs:label ?label
+                              }
+                      ")
+hash['results']['bindings'].each do |resource|
+  key = resource['label']['value'].downcase
+  value = resource['class']['value']
+  FORMATS[key] = value
+end
+
+
+PROPERTIES = {}
+hash = Ontology.query(" PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+                        PREFIX owl: <http://www.w3.org/2002/07/owl#>
+                        SELECT ?property ?label
+                        WHERE { ?property a owl:ObjectProperty .
+                                ?property rdfs:label ?label
+                              }
+                      ")
+hash['results']['bindings'].each do |resource|
+  key = resource['label']['value'].downcase
+  value = resource['property']['value']
+  PROPERTIES[key] = value
+end
+
+
+MODELS = {
   "http://www.owl-ontologies.com/book.owl#Author" => Author,
   "http://www.owl-ontologies.com/book.owl#Book" => Book,
   "http://www.owl-ontologies.com/book.owl#Edition" => Edition,
